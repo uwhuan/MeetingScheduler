84e640160776e7bad508b11c1d859dc0b83d1e3b
ice2meu <whuan@uw.edu>
2020-05-31 15:55:25 -0700

    Add meeting service

--- servers/group/model/schedule.go
+++ servers/group/model/schedule.go
@@ -1,12 +1,10 @@
 package model
 
-import "time"
-
 // Schedule struct stores a slot of time for a user for a meeting.
 // It is used to determine the final meeting time
 type Schedule struct {
-	ScheduleID int       `json:"scheduleID"`
-	StartTime  time.Time `json:"startTime"`
-	EndTime    time.Time `json:"endTime"`
-	Votes      int       `json:"votes"`
+	ScheduleID int    `json:"scheduleID"`
+	StartTime  string `json:"startTime"`
+	EndTime    string `json:"endTime"`
+	Votes      int    `json:"votes"`
 }
