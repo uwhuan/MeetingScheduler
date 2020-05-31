25098a122cec099ae23b5458aac23c210bdc9a2a
ice2meu <whuan@uw.edu>
2020-05-31 10:56:09 -0700

    Add store interface

--- servers/group/model/meetings.go
+++ servers/group/model/meetings.go
@@ -4,10 +4,11 @@ import "time"
 
 // Meeting struct contains information about a meeting
 type Meeting struct {
-	MeetingID  int       `json:"meetingID"`
+	MeetingID  int64     `json:"meetingID"`
 	Name       string    `json:"name"`
-	Creator    int       `json:"creator"`
-	Schedule   Schedule  `json:"-"`
+	Creator    int64     `json:"creator"`
+	StartTime  time.Time `json:"startTime"`
+	EndTime    time.Time `json:"endTime"`
 	CreateDate time.Time `json:"createDate"`
 	Confirmed  bool      `json:"confirmed"` // defalt false
 }
--- servers/group/model/schedule.go
+++ servers/group/model/schedule.go
@@ -2,19 +2,11 @@ package model
 
 import "time"
 
-// ScheduleID is the id of schedule
-type ScheduleID int
-
 // Schedule struct stores a slot of time for a user for a meeting.
 // It is used to determine the final meeting time
 type Schedule struct {
-	SID       ScheduleID `json:"scheduleID"`
-	StartTime time.Time  `json:"startTime"`
-	EndTime   time.Time  `json:"endTime"`
-}
-
-// Votes countes the number vote for certain schedule
-type Votes struct {
-	SID   ScheduleID `json:"scheduleID"`
-	Count int        `json:"Count"`
+	ScheduleID int       `json:"scheduleID"`
+	StartTime  time.Time `json:"startTime"`
+	EndTime    time.Time `json:"endTime"`
+	Votes      int       `json:"votes"`
 }
