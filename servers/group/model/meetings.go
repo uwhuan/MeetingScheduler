d95fc559e03857c99587d7f788ade0b9f9d0bf1f
ice2meu <whuan@uw.edu>
2020-05-31 08:32:26 -0700

    Add basic models struct

--- servers/group/model/meetings.go
+++ servers/group/model/meetings.go
@@ -1,3 +1,13 @@
 package model
 
+import "time"
+
 // Meeting struct contains information about a meeting
+type Meeting struct {
+	MeetingID  int       `json:"meetingID"`
+	Name       string    `json:"name"`
+	Creator    int       `json:"creator"`
+	Schedule   Schedule  `json:"-"`
+	CreateDate time.Time `json:"createDate"`
+	Confirmed  bool      `json:"confirmed"` // defalt false
+}
