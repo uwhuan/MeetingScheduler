823e43c1b5f8f6f2fdf1eb115abc2260a598b369
ice2meu <whuan@uw.edu>
2020-05-31 15:55:25 -0700

    Add meeting service

--- servers/group/model/meetings.go
+++ servers/group/model/meetings.go
@@ -1,14 +1,14 @@
 package model
 
-import "time"
-
 // Meeting struct contains information about a meeting
 type Meeting struct {
-	MeetingID  int64     `json:"meetingID"`
-	Name       string    `json:"name"`
-	Creator    int64     `json:"creator"`
-	StartTime  time.Time `json:"startTime"`
-	EndTime    time.Time `json:"endTime"`
-	CreateDate time.Time `json:"createDate"`
-	Confirmed  bool      `json:"confirmed"` // defalt false
+	MeetingID   int64  `json:"meetingID"`
+	GroupID     int64  `json:"groupID"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Creator     int64  `json:"creator"`
+	StartTime   string `json:"startTime"`
+	EndTime     string `json:"endTime"`
+	CreateDate  string `json:"createDate"`
+	Confirmed   bool   `json:"confirmed"` // defalt false
 }
