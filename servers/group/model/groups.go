7351d502480a8b7dba89508342d60e217d610f73
ice2meu <whuan@uw.edu>
2020-05-31 18:24:14 -0700

    change schedule date types

--- servers/group/model/groups.go
+++ servers/group/model/groups.go
@@ -1,13 +1,10 @@
 package model
 
-import "time"
-
 //Group struct contains fileds of a group of users
-
 type Group struct {
-	GroupID     int64     `json:"groupID"`
-	Description string    `json:"description"`
-	Name        string    `json:"name"`
-	CreatorID   int64     `json:"creatorID"`
-	CreateDate  time.Time `json:"createDate"`
+	GroupID     int64  `json:"groupID"`
+	Description string `json:"description"`
+	Name        string `json:"name"`
+	CreatorID   int64  `json:"creatorID"`
+	CreateDate  string `json:"createDate"`
 }
--- servers/group/model/schedule.go
+++ servers/group/model/schedule.go
@@ -4,6 +4,7 @@ package model
 // It is used to determine the final meeting time
 type Schedule struct {
 	ScheduleID int    `json:"scheduleID"`
+	MeetingID  int    `json:"meetingID"`
 	StartTime  string `json:"startTime"`
 	EndTime    string `json:"endTime"`
 	Votes      int    `json:"votes"`
